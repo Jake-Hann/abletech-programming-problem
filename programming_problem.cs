33551fdc079a6c10eb7eb434ccdb26c68a02e4b5
Jake <55943667+Jake-Hann@users.noreply.github.com>
2021-09-15 21:11:14 +1200

    Solution for programming problem

---
+++ programming_problem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ProgrammingProblem
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Press any key to begin the programming problem...");
+            Console.ReadKey(true);
+            Console.WriteLine();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("FooBar");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Foo");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Bar");
+                }
+                else
+                {
+                    Console.WriteLine(i.ToString());
+                }
+            }
+
+            Console.Write("Press any key to exit...");
+            Console.ReadKey(true);
+            Environment.Exit(0);
+        }
+    }
+}
